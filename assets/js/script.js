2f6417dea23617a4261fcf8bec74fda0249316f5
Muhammad Danish <muhammadsalman.webdev@gmail.com>
2025-02-28 21:59:51 +0500

    Code Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const sonogramIcon = document.getElementById("sonogram-icon");
+const comTabContent = document.getElementById("com-tab-content-main");
+const mailIcon = document.getElementById("mail-icon");
+const mailTabContent = document.getElementById("mail-tab-content-main");
+
+sonogramIcon.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  comTabContent.classList.add("show");
+  mailTabContent.classList.remove("show");
+});
+mailIcon.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  mailTabContent.classList.add("show");
+  comTabContent.classList.remove("show");
+});
