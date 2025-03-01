1041654815c7c3b9147340d2da50e55dc533b7ce
Muhammad Danish <muhammadsalman.webdev@gmail.com>
2025-03-02 04:00:55 +0500

    Animation Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,13 @@ allTabBtns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     // Removing all the tabs class
     allTabs.forEach((_tab) => _tab.classList.remove("show"));
+    allTabBtns.forEach((_btn) => _btn.classList.remove("active"));
 
     // Add the only one class
-    allTabs[index].classList.add("show");
+    const id = btn.getAttribute("data-tab-btn");
+    const curTab = document.querySelector(`[data-content-tab='${id}']`);
+
+    btn.classList.add("active");
+    curTab.classList.add("show");
   });
 });
